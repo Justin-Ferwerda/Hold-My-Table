0fd81ade32b1f3113e88358df9577a64f98cb2f6
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-02-11 13:59:25 -0600

    added cards on the dom for restaurants on landing page

--- hold-my-table-FE/components/restaurants/MainRestaurantCard.js
+++ hold-my-table-FE/components/restaurants/MainRestaurantCard.js
@@ -1,33 +1,35 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
-import Image from 'next/image';
 import { Rating } from 'react-simple-star-rating';
 
 export default function MainRestaurantCard({ restaurant, src }) {
-  <div className="all-restaurants-card">
-    <Card className="text-center restaurant-card">
-      <Image className="card-img-top" src={src} alt="Card image cap" />
-      <Card.Body className="restaurant-card-body">
-        <div className="starRating">
-          Rating:
-          <Rating
-            allowHover={false}
-            size={26}
-            allowFraction
-            iconsCount={5}
-            ratingValue={restaurant.rating}
-            readonly
-            tooltipStyle={{
-              height: 'auto', width: 'auto', fontSize: '13px', padding: '2px 4px', textAlign: 'center', marginTop: '4px', marginLeft: '10px',
-            }}
-          />
-        </div>
-        <Card.Text>{restaurant.name}</Card.Text>
-        <Card.Text>{restaurant.style.label} {restaurant.price_tier}</Card.Text>
-      </Card.Body>
-    </Card>
-  </div>;
+  return (
+    <div className="all-restaurants-card">
+      <Card className="text-center restaurant-card">
+        <img className="card-img-top" src={src} />
+        <Card.Body className="restaurant-card-body">
+          <div className="starRating">
+            <Rating
+              allowHover={false}
+              size={26}
+              allowFraction
+              iconsCount={5}
+              initialValue={restaurant.rating}
+              readonly
+              tooltipStyle={{
+                height: 'auto', width: 'auto', fontSize: '13px', padding: '2px 4px', textAlign: 'center', marginTop: '4px', marginLeft: '10px',
+              }}
+            />
+          </div>
+          <Card.Text>{restaurant.name}</Card.Text>
+          <Card.Text>{restaurant.style.label} {restaurant.price_tier}</Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 MainRestaurantCard.propTypes = {
@@ -38,5 +40,6 @@ MainRestaurantCard.propTypes = {
     }),
     price_tier: PropTypes.string,
     rating: PropTypes.number,
-  }),
+  }).isRequired,
+  src: PropTypes.string.isRequired,
 };
