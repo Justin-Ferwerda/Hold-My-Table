1cbae85d31305d6d06b13f8cb6e04b973e763339
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-02-11 13:09:35 -0600

    changed navbar no styling

--- hold-my-table-FE/components/NavBar.js
+++ hold-my-table-FE/components/NavBar.js
@@ -1,37 +1,112 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
 import Link from 'next/link';
-import {
-  Navbar, //
-  Container,
-  Nav,
-  Button,
-} from 'react-bootstrap';
+import { Avatar } from '@mui/material';
+import { Image } from 'mui-image';
+import { useRouter } from 'next/router';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
 import { signOut } from '../utils/auth';
+import { useAuth } from '../utils/context/authContext';
 
-export default function NavBar() {
+export default function MenuAppBar() {
+  const [auth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const { user } = useAuth();
+  const router = useRouter();
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const signMeOut = () => {
+    signOut();
+    router.push('/');
+  };
+
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Link passHref href="/">
-          <Navbar.Brand>CHANGE ME</Navbar.Brand>
-        </Link>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
-            <Link passHref href="/">
-              <Nav.Link>Home</Nav.Link>
-            </Link>
-            <Link passHref href="/delete-me">
-              <Nav.Link>Delete Me</Nav.Link>
-            </Link>
-            <Button variant="danger" onClick={signOut}>
-              Sign Out
-            </Button>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Box sx={{ display: 'flex' }}>
+      <AppBar position="static">
+        <Toolbar className="navbar">
+          <Link href="/" passHref>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 5 }}
+            >
+              <Image src="/images/navlogo.png" style={{ height: 120, width: 120 }} />
+            </IconButton>
+          </Link>
+          <FormControl style={{ width: 250 }}>
+            <InputLabel id="demo-simple-select-label">City</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value="nashville"
+              label="city"
+              onChange={() => {}}
+            >
+              <MenuItem value={10}>Nashville</MenuItem>
+              <MenuItem value={20}>New York</MenuItem>
+              <MenuItem value={30}>Los Angeles</MenuItem>
+            </Select>
+          </FormControl>
+          <Box
+            component="form"
+            sx={{
+              '& > :not(style)': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField id="outlined-basic" label="Search" variant="outlined" />
+          </Box>
+          {auth && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <Avatar alt={user.firstName} src={user.profileImageUrl} />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem className="signOut-btn" onClick={signMeOut}>Sign Out</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
