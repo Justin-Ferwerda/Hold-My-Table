a8b2e79216933d321732214cce0ed806081b30f1
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-03-01 12:51:20 -0600

    styled home page with scrolling and centered layout key

--- hold-my-table-FE/pages/index.js
+++ hold-my-table-FE/pages/index.js
@@ -1,26 +1,39 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import { Button } from '@mui/material';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight'; import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
 import MainRestaurantCard from '../components/restaurants/MainRestaurantCard';
 import { useRestaurant } from '../utils/context/restaurantContext';
 
 function Home() {
   const { restaurants } = useRestaurant();
+  const cardContainer = document.querySelector('.restaurant-container');
+
+  const onClickRight = () => {
+    cardContainer.scrollBy({
+      left: cardContainer.clientWidth,
+      behavior: 'smooth',
+    });
+  };
+
+  const onClickLeft = () => {
+    cardContainer.scrollBy({
+      left: -cardContainer.clientWidth,
+      behavior: 'smooth',
+    });
+  };
 
   return (
-    <>
+    <div className="home-page">
       <div
-        className="text-center d-flex flex-row justify-content-center align-content-center restaurant-container"
-        style={{
-          height: '50vh',
-          padding: '30px',
-          maxWidth: '400px',
-          margin: '0 auto',
-        }}
+        className="restaurant-container"
       >
         {restaurants.map((restaurant) => (
           <MainRestaurantCard key={restaurant.id} restaurant={restaurant} src={restaurant.bannerPic} />
         ))}
       </div>
-    </>
+      <Button className="scroll-button" onClick={onClickLeft}><ArrowCircleLeftIcon fontSize="large" className="scroll-icons" /></Button>
+      <Button className="scroll-button-right" onClick={onClickRight}><ArrowCircleRightIcon fontSize="large" className="scroll-icons" /></Button>
+    </div>
 
   );
 }
