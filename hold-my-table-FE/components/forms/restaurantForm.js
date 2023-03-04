0e6310e5b70e01ef340026d3a7312e057e369395
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-03-04 14:07:03 -0600

    refactored restaurant form to include all information

--- hold-my-table-FE/components/forms/restaurantForm.js
+++ hold-my-table-FE/components/forms/restaurantForm.js
@@ -1,7 +1,8 @@
 import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
-import { Form, FloatingLabel, Button } from 'react-bootstrap';
+import { Form, FloatingLabel } from 'react-bootstrap';
+import { Button } from '@mui/material';
 import { createRestaurant, updateRestaurant } from '../../utils/data/api/restaurantData';
 import getStyles from '../../utils/data/api/styleData';
 
@@ -27,7 +28,7 @@ export default function RestaurantForm({ restaurant }) {
     if (restaurant.id) {
       updateRestaurant(formData).then(() => router.push(`/restaurants/account/${restaurant.adminUser.id}`));
     } else {
-      createRestaurant(formData).then(() => router.push(`/restaurants/account/${restaurant.adminUser.id}`));
+      createRestaurant(formData).then(window.confirm('account created!'));
     }
   };
 
@@ -59,10 +60,6 @@ export default function RestaurantForm({ restaurant }) {
             <Form.Control type="text" placeholder="Address" name="address" value={formData.address} onChange={handleChange} required />
           </FloatingLabel>
 
-          <FloatingLabel controlId="floatingInput1" label="Email" className="mb-3">
-            <Form.Control type="text" placeholder="Email" name="email" value={formData.email} onChange={handleChange} required />
-          </FloatingLabel>
-
           <FloatingLabel controlId="floatingInput1" label="Website URL" className="mb-3">
             <Form.Control type="text" placeholder="Website Url" name="websiteUrl" value={formData.websiteUrl} onChange={handleChange} required />
           </FloatingLabel>
@@ -74,11 +71,16 @@ export default function RestaurantForm({ restaurant }) {
           <FloatingLabel controlId="floatingInput1" label="Photo URL" className="mb-3">
             <Form.Control type="text" placeholder="Photo Url" name="bannerPic" value={formData.bannerPic} onChange={handleChange} required />
           </FloatingLabel>
+
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
             <Form.Label>Cancellation Policy</Form.Label>
             <Form.Control as="textarea" rows={3} onChange={handleChange} name="cancellationPolicy" value={formData.cancellationPolicy} />
           </Form.Group>
-          <h3>What Genre do you classify yourself as?</h3>
+
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Short Bio</Form.Label>
+            <Form.Control as="textarea" rows={3} onChange={handleChange} name="cancellationPolicy" value={formData.bio} />
+          </Form.Group>
           <FloatingLabel controlId="floatingSelect" label="style">
             <Form.Select aria-label="style" name="style" onChange={handleChange} className="mb-3" value={formData?.style?.id} required>
               <option value="">Select a Style</option>
@@ -89,7 +91,16 @@ export default function RestaurantForm({ restaurant }) {
               ))}
             </Form.Select>
           </FloatingLabel>
-          <Button type="submit" className="submitButton">{restaurant.id ? 'Save' : 'Create'} Account</Button>
+          <FloatingLabel controlId="floatingSelect" label="style">
+            <Form.Select aria-label="style" name="price_tier" onChange={handleChange} className="mb-3" value={formData?.priceTier} required>
+              <option value="">Select a Price Tier</option>
+              <option value="$">$</option>
+              <option value="$$">$$</option>
+              <option value="$$$">$$$</option>
+              <option value="$$$$">$$$$</option>
+            </Form.Select>
+          </FloatingLabel>
+          <Button type="submit" variant="contained" className="submitButton">{restaurant.id ? 'Save' : 'Create'} Account</Button>
         </Form>
       </div>
       <div className="restaurant-form-RS">
@@ -97,7 +108,7 @@ export default function RestaurantForm({ restaurant }) {
           {!restaurant.id ? (
             <>
               <h4>Hold My Table offers a custom layout designer in order for customers to be able to reserve any table in your restaurant. On the next page you will recreate your layout and be asked to put in some additional info for each table.</h4>
-              <Button className="add-restaurant-btn" type="submit">Go To Layout Designer</Button>
+              <Button variant="contained" className="add-restaurant-btn" type="submit">Go To Layout Designer</Button>
             </>
           ) : (
             <div />
